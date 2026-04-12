289a8b430dcb7f2f60a8a353c8d3bb3dd9e5b0a2
Ewan <ewanwd2008@gmail.com>
2026-04-12 20:21:06 +0100

    added shop and did some code tidying

--- main.py
+++ main.py
@@ -16,9 +16,11 @@ WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Shooter Mcgee")
 vignette = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
-font = pygame.font.Font("fonts/LuckiestGuy-Regular.ttf", 32)
+font = pygame.font.Font("fonts/LuckiestGuy-Regular.ttf", 28)
 
 player = Player(WIDTH//2, HEIGHT//2)
+game_state = "PLAY"
+
 bullets = []
 particles = []
 enemies = []
@@ -32,6 +34,14 @@ wave_score = 0
 score = 1
 score_scale = 1
 display_score = 0
+shop_timer = 0
+shop_delay = 120
+going_to_shop = False
+
+damage = 1
+
+shop_buttons = [{"rect": pygame.Rect(100,200,250,60), "text": "Damage +1", "cost": 100, "action": "damage"},
+                {"rect": pygame.Rect(100,280,250,60), "text": "Speed +0.2", "cost": 100, "action": "speed"}]
 
 def create_vignette(surface):
     width, height = surface.get_size()
@@ -57,22 +67,26 @@ def spawn_wave(wave):
         new_enemies.append(Enemy(x, y, health))
     return new_enemies
 
+def handle_shop_click(pos):
+    global score, damage
+
+    for button in shop_buttons:
+        if button["rect"].collidepoint(pos):
+            if score >= button["cost"]:
+                score -= button["cost"]
+
+                if button["action"] == "damage":
+                    damage += 1
+                    button["cost"] += 50
+                
+                if button["action"] == "speed":
+                    player.speed += 0.2
+                    button["cost"] += 50
 #Main loop
 create_vignette(vignette)
 wave_in_progress = False
 running = True
 while running:
-
-    if not wave_in_progress:
-        wave_timer += 1
-        if wave_timer >= wave_delay:
-            wave_score += wave * 50
-            popups.append(Popup(WIDTH//2 - 36, HEIGHT-500, f"ROUND: {wave}"))
-            popups.append(Popup(WIDTH//2 - 36, HEIGHT-450, f"+ {wave_score}"))
-            score += wave_score
-            enemies = spawn_wave(wave)
-            wave_in_progress = True
-            wave_timer = 0
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -80,112 +94,191 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+            if event.key == pygame.K_RETURN and game_state == "SHOP":
+                game_state = "PLAY"
+                wave += 1
+                wave_in_progress = False
+
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                mouse_x, mouse_y = pygame.mouse.get_pos()
-                bullets.append(Bullet(player.pos.x, player.pos.y, mouse_x, mouse_y))
-
-    player.update()
-#------Enemy------
-    for enemy in enemies:
-        enemy.update(player.pos)
+                if game_state == "PLAY":
+                    mouse_x, mouse_y = pygame.mouse.get_pos()
+                    bullets.append(Bullet(player.pos.x, player.pos.y, mouse_x, mouse_y))
+                elif game_state == "SHOP":
+                    handle_shop_click(pygame.mouse.get_pos())
     
-    if wave_in_progress and len(enemies) == 0:
-        wave += 1
-        wave_in_progress = False
+    if game_state == "PLAY":
+        if not wave_in_progress:
+            wave_timer += 1
+            if wave_timer >= wave_delay:
+                wave_score += wave * 50
+                popups.append(Popup(WIDTH//2 - 36, HEIGHT-500, f"ROUND: {wave}"))
+                popups.append(Popup(WIDTH//2 - 36, HEIGHT-450, f"+ {wave_score}"))
+                score += wave_score
+                enemies = spawn_wave(wave)
+                wave_in_progress = True
+                wave_timer = 0
 
+        player.update()
+#------Enemy------
+        for enemy in enemies:
+            enemy.update(player.pos)
+        
 #------Bullets------
-    for bullet in bullets:
-        bullet.update()
+        for bullet in bullets:
+            bullet.update()
 
 #------Popups------
-    for popup in popups:
-        popup.update()
-    
-    popups = [p for p in popups if not p.is_dead()]
-      
-#------Collisions------
-    new_enemies = []
-    new_bullets = []
-
-    for enemy in enemies:
-        hit = False
-
-        for bullet in bullets:
-            if bullet.pos.distance_to(enemy.pos) < enemy.radius:
-                enemy.health -= 1
-                bullets.remove(bullet)
-                if enemy.health >=1:
-                    score += 1
-                    popups.append(Popup(enemy.pos.x, enemy.pos.y, "+1"))
-
-                for _ in range(8):
-                    particles.append(Collision(enemy.pos.x, enemy.pos.y))
-
-                if enemy.health <= 0:
-                    shake_strength = 8
-                    for _ in range(12): 
-                        particles.append(Collision(enemy.pos.x, enemy.pos.y))
-
-                    popups.append(Popup(enemy.pos.x, enemy.pos.y, "+1"))
-                    score += 1
-                    score_scale = 1.5
-
-                    hit = True
-                break
+        for popup in popups:
+            popup.update()
         
-        if not hit:
-            new_enemies.append(enemy)
-
-    for bullet in bullets:
-        hit_any = False
+        popups = [p for p in popups if not p.is_dead()]
+        
+#------Collisions------
+        new_enemies = []
+        new_bullets = []
 
         for enemy in enemies:
-            if bullet.pos.distance_to(enemy.pos) < enemy.radius:
+            hit = False
+
+            for bullet in bullets:
+                if bullet.pos.distance_to(enemy.pos) < enemy.radius:
+                    enemy.health -= damage
+                    bullets.remove(bullet)
+                    if enemy.health >=1:
+                        score += 1
+                        popups.append(Popup(enemy.pos.x, enemy.pos.y, "+1"))
+
+                    for _ in range(8):
+                        particles.append(Collision(enemy.pos.x, enemy.pos.y))
+
+                    if enemy.health <= 0:
+                        shake_strength = 8
+                        for _ in range(12): 
+                            particles.append(Collision(enemy.pos.x, enemy.pos.y))
+
+                        popups.append(Popup(enemy.pos.x, enemy.pos.y, "+1"))
+                        score += 1
+                        score_scale = 1.5
+
+                        hit = True
+                    break
+            
+            if not hit:
+                new_enemies.append(enemy)
+
+        for bullet in bullets:
+            hit_any = False
+
+            for enemy in enemies:
+                if bullet.pos.distance_to(enemy.pos) < enemy.radius:
+                    hit_any = True
+                    break
+            if bullet.hit_wall(WIDTH, HEIGHT):
+                for _ in range(10):
+                    particles.append(Collision(bullet.pos.x, bullet.pos.y))
                 hit_any = True
-                break
-        if bullet.hit_wall(WIDTH, HEIGHT):
-            for _ in range(10):
-                particles.append(Collision(bullet.pos.x, bullet.pos.y))
-            hit_any = True
-        if not hit_any:
-            new_bullets.append(bullet)
+            if not hit_any:
+                new_bullets.append(bullet)
+        
+        bullets = new_bullets
+        enemies = new_enemies
     
-    bullets = new_bullets
-    enemies = new_enemies
- 
 #------Particles------
-    for particle in particles:
-        particle.update()
-    
-    particles = [p for p in particles if not p.is_dead()]
+        for particle in particles:
+            particle.update()
+        
+        particles = [p for p in particles if not p.is_dead()]
+
+#------Game states------
+        if game_state == "PLAY":
+            if wave_in_progress and len(enemies) == 0 and not going_to_shop:
+                going_to_shop = True
+                shop_timer = 0
+
+    if going_to_shop:
+        shop_timer += 1
+
+        for particle in particles:
+            particle.update()
+        particles = [p for p in particles if not p.is_dead()]
+        
+        for popup in popups:
+            popup.update()
+        popups = [p for p in popups if not p.is_dead()]
+
+        if shop_timer >= shop_delay:
+            game_state = "SHOP"
+            going_to_shop = False
+            wave_in_progress = False
+            wave += 1
+
+    elif game_state == "SHOP":
+        pass
 
 #------Camera------
     offset_x = random.uniform(-shake_strength, shake_strength)
     offset_y = random.uniform(-shake_strength, shake_strength)
+    shake_strength *= 0.9
 
-#------Score------
-    display_score += (score - display_score) * 0.1
-    score_scale += (1 - score_scale) * 0.1
 
 #------Drawing------
     screen.fill((30,30,30))
+    if game_state == "PLAY":
 
-    player.draw(screen, offset_x, offset_y)
-    for bullet in bullets:
-        bullet.draw(screen, offset_x, offset_y)
+        player.draw(screen, offset_x, offset_y)
+        for bullet in bullets:
+            bullet.draw(screen, offset_x, offset_y)
+        
+        for particle in particles:
+            particle.draw(screen, offset_x, offset_y)
+
+        for enemy in enemies:
+            enemy.draw(screen, offset_x, offset_y)
     
-    for particle in particles:
-        particle.draw(screen, offset_x, offset_y)
+        for popup in popups:
+            popup.draw(screen)
 
-    for enemy in enemies:
-        enemy.draw(screen, offset_x, offset_y)
-  
-    for popup in popups:
-        popup.draw(screen)
+    elif game_state == "SHOP":
+        mouse_pos = pygame.mouse.get_pos()
+        screen.fill((30,75,30))
 
-    pygame.draw.rect(screen, (20,20,20), (5,5,180,100), border_radius=8)
-    pygame.draw.rect(screen, (200,200,200), (5,5,180,100), 2, border_radius=8)
+        title = font.render("SHOP - PRESS ENTER TO CONTINUE", True, (255,255,255))
+        screen.blit(title, (250, 50))
+
+        for button in shop_buttons:
+
+            if button["rect"].collidepoint(mouse_pos):
+                colour = (80,80,80)
+            else:
+                colour = (40,40,40)
+            
+            if score < button["cost"]:
+                colour = (25,25,25)
+
+            pygame.draw.rect(screen, (40,40,40), button["rect"], border_radius=8)
+            pygame.draw.rect(screen, (200,200,200), button["rect"], 2, border_radius=8)
+
+            text = font.render(f"{button['text']} (${button['cost']})", True, (255,255,255))
+            screen.blit(text, (button["rect"].x + 10, button["rect"].y + 15))
+        
+
+#------Cash board------
+
+    display_score += (score - display_score) * 0.1
+    score_scale += (1 - score_scale) * 0.1
+
+    board_width = 180
+    max_board_width = 320
+
+    board_target = board_width + int(display_score * 0.1)
+    board_target = min(board_target, max_board_width)
+
+    box_width = 180
+    box_width += (board_target - box_width) * 0.1
+
+    pygame.draw.rect(screen, (20,20,20), (5,5,int(box_width),100), border_radius=8)
+    pygame.draw.rect(screen, (200,200,200), (5,5,int(box_width),100), 2, border_radius=8)
     screen.blit(vignette, (0,0))
 
     score_text = font.render(f"Cash: {int(display_score)}", True, (200,200,200))
